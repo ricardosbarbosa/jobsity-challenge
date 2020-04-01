6e1fea2cdbc12457c3ff7827952d60135f4a4baa
Ricardo Barbosa <rbrico@gmail.com>
2020-04-01 20:30:42 -0300

    React color

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,6 +3,7 @@ import { Formik, Form, Field } from 'formik';
 import { Button, LinearProgress } from '@material-ui/core';
 import { TextField } from 'formik-material-ui';
 import { DateTimePicker } from 'formik-material-ui-pickers';
+import { TwitterPicker } from 'react-color';
 
 function EventForm() {
   return (
@@ -10,7 +11,7 @@ function EventForm() {
        <h2>Event</h2>
     
     <Formik
-      initialValues={{ reminder: '', datetime: null, city: ''}}
+      initialValues={{ reminder: '', start: null, end: null, city: '', color: '#3174ad'}}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           setSubmitting(false);
@@ -18,11 +19,32 @@ function EventForm() {
         }, 500);
       }}
     >
-      {({ submitForm, isSubmitting }) => (
+      {({ values, submitForm, isSubmitting, setFieldValue }) => (
         <Form >
+          {JSON.stringify(values)}
+          <div style={{ backgroundColor: values.color, padding: 2, color: 'white',
+            border:'none',
+            boxSizing: 'border-box',
+            boxShadow:'none',
+            margin:'0',
+            padding: '2px 5px',
+            borderRadius:'5px', 
+            cursor:'pointer',
+            width:'100%',
+            textAlign:'left',
+          }}>
+            {values.reminder} - {values.city}
+          </div>
           <Field component={TextField} fullWidth name="reminder" type="text" label="Reminder" variant="outlined" margin="normal" size="small"  multiline rows={2} inputProps={{ maxLength: 30}} />
           <Field component={TextField} fullWidth name="city" type="text" label="City" variant="outlined" margin="normal" size="small"/>
-          <Field component={DateTimePicker} fullWidth name="datetime" label="Date Time" inputVariant="outlined" margin="normal" size="small"/>
+          <Field component={DateTimePicker} fullWidth name="start" label="Start Date Time" inputVariant="outlined" margin="normal" size="small"/>
+          <Field component={DateTimePicker} fullWidth name="end" label="End Date Time" inputVariant="outlined" margin="normal" size="small"/>
+          <TwitterPicker width="100%" style={{justifyContent: 'center'}} triangle='hide' onChange={(color ) => {
+            setFieldValue('color', color.hex)
+          }}/>
+          <h5>Weather: <span>Rain</span></h5>
+
+          
           {isSubmitting && <LinearProgress />}
           <Button variant="contained" color="primary" disabled={isSubmitting} onClick={submitForm}  margin="normal" size="small">
             Submit
