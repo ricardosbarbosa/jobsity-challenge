8414e8ff492f03d218718c3cc2ef223620983a23
Ricardo Barbosa <rbrico@gmail.com>
2020-04-02 19:29:59 -0300

    https

--- src/api/weatherapi.js
+++ src/api/weatherapi.js
@@ -4,7 +4,7 @@ import { fromApiGetWeather } from "./normalize/weatherapi"
 
 export const getWeatherByCityAndDate = (city, date) => {
   const key = 'fa13689ac4124371a83205451200204'
-  const url = `http://api.weatherapi.com/v1/history.json?key=${key}&q=${city}&dt=${moment(date).format('YYYY-MM-DD')}`
+  const url = `https://api.weatherapi.com/v1/history.json?key=${key}&q=${city}&dt=${moment(date).format('YYYY-MM-DD')}`
   return Axios.get(url)
     .then(response => fromApiGetWeather(response.data))
 }
